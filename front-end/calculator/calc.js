47a21ce0a3405344570f63763a14f027f4589c41
Howard Shaw <chip.shaw4@gmail.com>
2017-08-15 19:19:38 -0600

    made small adjustment to how clear button updates, sometimes pressing 0 button should update clear button

--- front-end/calculator/calc.js
+++ front-end/calculator/calc.js
@@ -43,7 +43,7 @@
     /*****************
         Data
     ******************/
-    var state = {
+    /*var*/ state = {
 
         //user interface
         isFocused: false,       //True when a button has been clicked/touched and not released 
@@ -89,19 +89,24 @@
 
     //adds event listeners to node list (nodes arg), events arg is array
     function addListenerToList(nodes, events, callback){
+        //not calling for each directly on nodes because it is an
+        //HTMLCollection and doesn't have a forEach Method
         Array.prototype.forEach.call(nodes, function(n){
             events.forEach( function(e) { n.addEventListener(e, callback)});
         });
     }
 
-    //returns first node frome list that matches given value for given html data attribute
+    //returns node from list that matches value for given html data attribute
+    //(there should only be one)
     function getButtonFromData(nodes, dataAttr, value){
         var match;
+        //not calling for each directly on nodes because it is an
+        //HTMLCollection and doesn't have a forEach Method
         Array.prototype.forEach.call(nodes, function(n){
             if (n.dataset[dataAttr] === value){
                 match = n;
             }
-        })
+        });
         return match;
     }
 
@@ -110,12 +115,12 @@
     /*****************
         Mouse Events
     ******************/
+
     //done to emulate finger pressing and moving on phone screen
-    //touching a button highlights it
-    //moving finger to other buttons highlight them instead
-    //lifting finger triggers button that finger is on 
+    //clicking a button highlights it 
     function handleMouseDown(e){
         e.preventDefault();
+        //if there are no other active "pointer events"
         if( !state.isFocused ){
             playTap();
             e.currentTarget.classList.add('focused');
@@ -123,6 +128,7 @@
             state.focusType = 'mouse';
         }
     }
+    //moving pointer to other buttons highlight them instead
     function handleMouseEnter(e){
         if( state.isFocused && state.focusType === 'mouse'){
             e.currentTarget.classList.add('focused');
@@ -133,11 +139,17 @@
             e.currentTarget.classList.remove('focused');
         }
     }
+    //lifting pointer triggers button that pointer is on 
     function handleMouseUp(e){
-        e.stopPropagation(); //don't run this from document after a button runs it
+        //don't run this from document after a button runs it
+        //since there are listeners with this callback on both
+        e.stopPropagation(); 
+
         if( state.isFocused && state.focusType === 'mouse'){
             state.isFocused = false;
             state.focusType = null;
+
+            //use button
             var target = e.currentTarget;
             if(target !== document){
                 target.classList.remove('focused');
@@ -156,8 +168,10 @@
     /*****************
         Touch Events
     ******************/
+    //touching a button highlights it 
     function handleTouchStart(e){
         e.preventDefault();
+        //if there are no other active "pointer events"
         if( !state.isFocused ){
             playTap();
             e.currentTarget.classList.add('focused');
@@ -165,16 +179,25 @@
             state.focusType = 'touch';
             state.touchId = e.changedTouches[0].identifier;
             state.touchedButton = e.currentTarget;
+
+            //store coordinates of current button so we know when the touch
+            //moves off of this button
             state.buttonCoords = getPageCoords(e.currentTarget);
+            //get coordinates of top left button and button width/height
+            //to use for calculating which button is under given page coordinates
             state.topLeftButton = getPageCoords(buttons[0], true);
         }
     }
+    //moving finger to other buttons highlight them instead
     function handleTouchMove(e){
         if( state.isFocused && state.focusType === 'touch'){
             var touch = e.changedTouches[0];
+            //if touch that has moved is the active touch
             if (touch.identifier === state.touchId){
                 var coords = state.buttonCoords;
+                //if the touch was not previously on a button
                 if (state.touchedButton === null){
+                    //see if the touch is currently on a button
                     state.touchedButton = getButtonFromCoords(touch.pageX, touch.pageY);
                     if( state.touchedButton !== null){
                         state.touchedButton.classList.add('focused');
@@ -184,10 +207,11 @@
                         state.buttonCoords = null;
                     }
                 }
+                //if the touch has exited the boundaries of the previously touched button
                 else if( touch.pageY < coords.top || touch.pageY > coords.bottom ||
-                    touch.pageX < coords.left || touch.pageX > coords.right 
-                  ){
+                touch.pageX < coords.left || touch.pageX > coords.right ){
                     state.touchedButton.classList.remove('focused');
+                    //see if it's on a different button
                     state.touchedButton = getButtonFromCoords(touch.pageX, touch.pageY);
                     if( state.touchedButton !== null){
                         state.touchedButton.classList.add('focused');
@@ -200,10 +224,13 @@
             }
         }
     }
+    //lifting finger triggers button that finger is on 
     function handleTouchEnd(e){
         if( state.isFocused && state.focusType === 'touch'){
             var touch = e.changedTouches[0];
+            //if the touch is the active touch
             if (touch.identifier === state.touchId){
+                //if the touch was on a button, perform the action for that button
                 if (state.touchedButton !== null){
                     state.touchedButton.classList.remove('focused');
                     useButton(state.touchedButton);
@@ -217,6 +244,7 @@
             }
         }
     }
+    //for touch cancel, remove focus style but don't perform button action 
     function handleTouchCancel(e){
         if( state.isFocused && state.focusType === 'touch'){
             var touch = e.changedTouches[0];
@@ -238,29 +266,34 @@
     document.addEventListener('touchend', handleTouchEnd);
     document.addEventListener('touchcancel', handleTouchCancel);
 
-
-    function getPageCoords(element, widthHeight){
+    //returns an object containing coordinates for a given HTML element.
+    //returnWandH indicates whether or not we need to add the width and 
+    //height of the element as properties on the returned object
+    function getPageCoords(element, returnWandH){
         var vpCoords = element.getBoundingClientRect();
         var pageCoords = {};
         pageCoords.top = vpCoords.top + pageYOffset;
         pageCoords.bottom = vpCoords.bottom + pageYOffset;
         pageCoords.left = vpCoords.left + pageXOffset;
         pageCoords.right = vpCoords.right + pageXOffset;
-        if(widthHeight === true){
+        if(returnWandH === true){
             pageCoords.width = vpCoords.width;
             pageCoords.height = vpCoords.height;
         }
         return pageCoords;
     }
 
+    //returns currently touched button from provided page coordinates
     function getButtonFromCoords(x, y){
         var coords = state.topLeftButton;
         var row = Math.floor((y - coords.top) / coords.height);
         var column = Math.floor((x - coords.left) / coords.width); 
+        //if coordinates are outside the container of buttons 
         if( row < 0 || row > 4 || column < 0 || column > 3){
             return null;
         }
         var index = row * 4 + column;
+        //because the 0 button is double wide and at index 16
         if (index >= 17){
             index -= 1;
         }
@@ -301,19 +334,34 @@
     }
 
     function useNumber(num){
+        //if the calculator is expecting a new number
+        //e.g. after clearing or clicking an operator button
         if (state.newInput){
             state.displayType = 'input';
-            state.input = [num];
-            state.inputLength = 1;
+
+            //using the number 0 doesn't change the clear button from AC to C
             if (num !== '0'){
                 state.newInput = false;
                 setClear('display');
             }
+            //examples: press +/- then 0;
+            // 3 + = C = 0;
+            // ÷ = 0
+            //unless the current display value is not 0 or is negative 0
+            else if(state.isNegative || num !== state.displayValue.toString()){
+                setClear('display');
+            }
+
+            //reset the input array and length to contain only the given digit
+            state.input = [num];
+            state.inputLength = 1;
             if (state.currentOperator !== null){
                 deactivateOperator();
             }
             updateInputValue();
         }
+        //otherwise, if the length of the currently input number isn't at
+        //the maximum number of digits (9)
         else if (state.inputLength < 9){
             state.input.push(num);
             state.inputLength += 1;
@@ -641,8 +689,10 @@
         if (type !== 'display' && type !== 'all'){
             throw new Error('Invalid value for clear: ' + type);
         }
-        state.clearType = type;
-        updateClearButton(type);
+        if( type !== state.clearType){
+            state.clearType = type;
+            updateClearButton(type);
+        }
     }
     function updateClearButton(type){
         if (type === 'all'){
